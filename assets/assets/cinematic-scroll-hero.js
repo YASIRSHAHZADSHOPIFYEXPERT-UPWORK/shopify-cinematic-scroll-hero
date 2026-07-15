f17e6a508f7c7bc063b41669f6de93e853d72d68
Yasir Shahzad <yasirhakeem560.yk@gmail.com>
2026-07-15 16:01:30 +0500

    Add hero script

---
+++ assets/assets/cinematic-scroll-hero.js
@@ -0,0 +1,9 @@
+/*!
+==================================================
+Shopify Cinematic Scroll Hero
+Version: 1.0.0
+Author: Yasir Shahzad
+==================================================
+*/
+
+// Production JavaScript will be added here.
